3c10872dfe305256e0941dda8e2075b752690b22
Rocko <codingwithrock@gmail.com>
2022-09-20 17:52:23 +0600

    component are created

--- src/App.js
+++ src/App.js
@@ -14,11 +14,31 @@ function App() {
 
       {/* Adding CSS Style in three ways into Heading... */}
 
-      <h1 className='heading' style={{backgroundColor: 'red',color:'white'}}>User Data Base</h1>
-     
+      <h1 className='heading' style={{ backgroundColor: 'red', color: 'white' }}>User Data Base</h1>
+
+      {/* Second Core creating Component */}
+      <Users></Users>
+      <Users></Users>
+
 
     </div>
   );
 }
+
+
+
+const Users = () => {
+
+  return (
+    <div className='users' style={{marginTop:'10px', border:'2px solid black', borderRadius: '10px', margin:'10px'}}>
+      <h3>User Name: </h3>
+      <h4>User Email: </h4>
+      <p>Comments</p>
+    </div>
+  )
+}
+
+
+
 
 export default App;
